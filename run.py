15f665acccaa103c29ecb67810a0505bb8de02e2
Alexandre Misrahi <78612777+AlexandreMisrahi2005@users.noreply.github.com>
2023-11-23 14:06:20 +0100

    Update run.py

--- run.py
+++ run.py
@@ -13,10 +13,10 @@ if __name__ == "__main__":
 	noise = np.random.randn(m)
 	y = np.dot(X, true_theta) + noise
 
-	mcmc = MarkovChainMonteCarlo(X, y, beta=1.0, num_samples=1000)
-
 	initial_theta = np.random.randint(0, 2, size=d)
-	samples = mcmc.metropolis_hastings(initial_theta)
 
+	samples = None # compute samples
+
+	# compute MSE (q 1.5)
 	mse = 2 * np.mean(np.linalg.norm(samples.mean(axis=0) - true_theta)**2) / d
 	print(f"Mean Squared Error: {mse}")
