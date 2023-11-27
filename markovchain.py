c6a810a45430b1cb5cccc092790de9f4300e0fea
ris-iris <sagitova.ma@phystech.edu>
2023-11-27 19:05:18 +0100

    refactor acceptance probability calculator

--- markovchain.py
+++ markovchain.py
@@ -19,13 +19,13 @@ class MarkovChain(ABC):
         pass
 
     @abstractmethod
-    def proposed_state(self):
+    def accept_proposed_state(self):
         pass
 
     def update_state(self):
-        new_state, acceptance_prob = self.propose()
-        if np.random.rand() < acceptance_prob:
-            self.current_state = new_state
+        self.propose()
+        if np.random.rand() < self.acceptance_probability():
+            self.accept_proposed_state()
 
 
 class AcceptanceCalculator:
@@ -34,14 +34,18 @@ class AcceptanceCalculator:
         self.y = y  # Observed measurements
         self.beta = beta  # Inverse temperature
         self.noise = None
+        self.proposed_noise = None
 
     def acceptance(self, theta, flip_idx):
+        if self.noise is None:
+            self.noise = self.X @ theta - self.y
+
         theta_idx = theta[flip_idx]
         coef = 1 - 2 * theta_idx
 
         X_idx = self.X[:, flip_idx]
 
-        self.noise = self.X @ theta - self.y if self.noise is None else self.noise + coef * X_idx
+        self.proposed_noise = self.noise + coef * X_idx
 
         power = -self.beta * coef * np.dot(2 * self.noise + coef * X_idx, X_idx)
         if power >= 0:
@@ -49,17 +53,23 @@ class AcceptanceCalculator:
         return np.exp(power)
 
     def swap_acceptance(self, theta, flip_one_idx, flip_zero_idx):
-        noise = self.X @ theta - self.y
+        if self.noise is None:
+            self.noise = self.X @ theta - self.y
 
         X_one_idx = self.X[:, flip_one_idx]
         X_zero_idx = self.X[:, flip_zero_idx]
 
         X_idx = X_zero_idx - X_one_idx
 
-        power = -self.beta * np.dot(2 * noise + X_idx, X_idx)
+        self.proposed_noise = self.noise + X_idx
+
+        power = -self.beta * np.dot(2 * self.noise + X_idx, X_idx)
         if power >= 0:
             return 1
         return np.exp(power)
+    
+    def update_noise(self):
+        self.noise = self.proposed_noise
 
 
 class BinaryHypercubeChain(MarkovChain):
@@ -73,17 +83,18 @@ class BinaryHypercubeChain(MarkovChain):
             else np.random.randint(0, 2, size=d)
         )
 
+        self.flip_idx = None
+
     def propose(self):
-        flip_idx = np.random.randint(self.d)
-        return self.proposed_state(flip_idx), self.acceptance_probability(flip_idx)
+        self.flip_idx = np.random.randint(self.d)
 
-    def acceptance_probability(self, flip_idx):
-        return self.acceptance_calc.acceptance(self.current_state, flip_idx)
+    def acceptance_probability(self):
+        return self.acceptance_calc.acceptance(self.current_state, self.flip_idx)
 
-    def proposed_state(self, flip_idx):
-        theta = self.current_state.copy()
-        theta[flip_idx] = 1 - self.current_state[flip_idx]
-        return theta
+    def accept_proposed_state(self):
+        self.current_state[self.flip_idx] = 1 - self.current_state[self.flip_idx]
+
+        self.acceptance_calc.update_noise()
 
 
 class SwapChain(MarkovChain):
@@ -97,22 +108,22 @@ class SwapChain(MarkovChain):
             else np.random.randint(0, 2, size=d)
         )
 
+        self.flip_one_idx, self.flip_zero_idx = None, None
+
     def propose(self):
         ones = np.argwhere(self.current_state == 1).reshape(-1)
         zeros = np.argwhere(self.current_state == 0).reshape(-1)
 
-        flip_one_idx = np.random.choice(ones)
-        flip_zero_idx = np.random.choice(zeros)
+        self.flip_one_idx = np.random.choice(ones)
+        self.flip_zero_idx = np.random.choice(zeros)
 
-        return self.proposed_state(flip_one_idx, flip_zero_idx), self.acceptance_probability(flip_one_idx, flip_zero_idx)
-
-    def acceptance_probability(self, flip_one_idx, flip_zero_idx):
+    def acceptance_probability(self):
         return self.acceptance_calc.swap_acceptance(
-            self.current_state, flip_one_idx, flip_zero_idx
+            self.current_state, self.flip_one_idx, self.flip_zero_idx
         )
 
-    def proposed_state(self, flip_one_idx, flip_zero_idx):
-        theta = self.current_state.copy()
-        theta[flip_zero_idx] = 1
-        theta[flip_one_idx] = 0
-        return theta
+    def accept_proposed_state(self):
+        self.current_state[self.flip_zero_idx] = 1
+        self.current_state[self.flip_one_idx] = 0
+
+        self.acceptance_calc.update_noise()
